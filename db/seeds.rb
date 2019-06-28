95b750d666ac91e865f1ae2fb2bb08c860c39ccb
Ankur <ankurmaurya1993@gmail.com>
2019-06-28 16:00:15 +0530

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Slider.create(heading: 'Why Artificial Intelligence Matters', paragraph: "The journey towards AI organisations is now an integral component of corporate strategy as companies focus on redesigning core systems, processes, and business strategies around AI and its possibilities.")
+Slider.create(heading: 'Marketing Trends 2019', paragraph: "There are many new digital marketing trends and strategies that are evolving in the current high-tech, Internet-connected era and businesses now need to use them to succeed in their efforts. Here are key digital marketing trends for 2019.")
+Slider.create(heading: 'Innovation Models', paragraph: "With industries from banking to healthcare being transformed and undermined by new business models and technology, executives are smart to wonder, “Are we next?” To confidently answer no, what you really need to do is put a system in place that allows them to continuously innovate.")
+User.create(email: 'ankurmaurya1993@gmail.com', password: "12345678")
